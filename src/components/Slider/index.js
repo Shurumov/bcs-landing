3b5b7c5a742187a0b32cdf4bd9be910fc97df472
Shurumov <alexei0113@gmail.com>
2020-03-06 23:57:45 +0700

    Добавлены переходы с цветами для слайдера

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,15 +1,16 @@
 import React, {PureComponent} from "react";
+import cn from 'classnames';
 import BaseSlider from '@farbenmeer/react-spring-slider';
+import data from './slides'
 import './slider.scss'
 
 const BulletComponent = (props) => {
-  const {onClick, isActive, bgc} = props;
+  const {onClick, bgc} = props;
   return (
     <div
-      className={bgc}
+      className={cn('slider__bullet', bgc)}
       style={{
         margin: '0 2px',
-        opacity: isActive && '0.5'
       }}
       onClick={onClick}
     >
@@ -37,24 +38,46 @@ export class Slider extends PureComponent {
       index: index
     })
   };
+
   render() {
-    const { index } = this.state;
+    const {index} = this.state;
     return (
-      <div style={{height: '100px'}}>
-        <BaseSlider
-          activeIndex={this.state.index}
-          hasBullets
-          onSlideChange={this.onSlideChange}
-          BulletComponent={props => BulletComponent({...props, bgc: this.colorsClass[index]})}
-          // ArrowComponent={ArrowComponent}
-        >
-          <div>child 1</div>
-          <div>child 2</div>
-          <div>child 3</div>
-          <div>child 4</div>
-          <div>child 5</div>
-          <div>child 6</div>
-        </BaseSlider>
+      <div
+        className={"container"}
+      >
+        <div className="slider">
+          <BaseSlider
+            activeIndex={this.state.index}
+            hasBullets
+            onSlideChange={this.onSlideChange}
+            // BulletComponent={props => BulletComponent({...props, bgc: this.colorsClass[index]})}
+            BulletComponent={() => <div/>}
+            // ArrowComponent={ArrowComponent}
+          >
+            {[0, 1, 2, 3, 4, 5].map(item => (
+              <div
+                className={cn('slider__item', this.colorsClass[item])}
+              >
+                <div className="content">
+                  item
+                </div>
+              </div>
+            ))}
+          </BaseSlider>
+          <div className="content">
+            <div className="slider__bullet_wrapper">
+              {(data || []).map((item, index) => (
+                <div
+                  className={cn('slider__bullet', this.colorsClass[index])}
+                  onClick={() => this.onSlideChange(index)}
+                  key={item + index}
+                >
+                  {data[index].link}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
